2307c2dc37d68341b71c32fe8b20f980ed8f5fe0
Francesco Pasqual <francesco.pasqual17@gmail.com>
2025-05-10 11:45:28 +0200

    file pom, checkstyle, sorgenti e test iniziali - close #1

---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////
+// Francesco Pasqual 2103119
+// Marco Piro 2068075
+////////////////////////////////////////
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
