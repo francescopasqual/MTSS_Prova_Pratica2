269a76b6b29d0bf2a2ae4a50e3305cd29849f6d1
Marco Piro <marco.piro.1@studenti.unipd.it>
2025-05-10 11:54:58 +0200

    checkstyle, header - close #2

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -1,4 +1,7 @@
-
+////////////////////////////////////////
+// Francesco Pasqual 2103119
+// Marco Piro 2068075
+////////////////////////////////////////
 package it.unipd.mtss;
 
 public class IntegerToRoman {
